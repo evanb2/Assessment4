9a289e0a374d7fb4191082768b5159c9818a159e
Evan Butler <evanb2@mac.com>
2015-03-27 14:18:25 -0700

    write routes for POST

--- app/app.php
+++ app/app.php
@@ -40,14 +40,44 @@
   });
 
   //store EDIT form
+  $app->get("/stores/{id}/edit", function($id) use ($app) {
+    $store = Store::find($id);
+    return $app['twig']->render('store_edit.twig', array('store' => $store));
+  });
 
   //CREATE store
+  $app->post("/stores", function() use ($app) {
+    $storename = $_POST['storename'];
+    $id = $_POST['id'];
+    $store = new Store($storename, $id);
+    $store->save();
+    return $app['twig']->render('stores.twig', array('stores' => Store::getAll()));
+  });
 
   //CREATE brand
+  $app->post("/brands", function() use ($app) {
+    $brandname = $_POST['brandname'];
+    $id = $_POST['id'];
+    $brand = new Brand($brandname, $id);
+    $brand->save();
+    return $app['twig']->render('brands.twig', array('brands' => Brand::getAll()));
+  });
 
   //CREATE add stores to brands
+  $app->post("/add_stores", function() use ($app) {
+    $store = Store::find($_POST['store_id']);
+    $brand = Brand::find($_POST['brand_id']);
+    $brand->addStore($store);
+    return $app['twig']->render('brand.twig', array('brand' => $brand, 'brands' => Brand::getAll(), 'stores' => $brand->getStores(), 'all_stores' => Store::getAll()));
+  });
 
   //CREATE add brands to stores
+  $app->post("/add_brands", function() use ($app) {
+    $store = Store::find($_POST['store_id']);
+    $brand = Brand::find($_POST['brand_id']);
+    $store->addBrand($brand);
+    return $app['twig']->render('store.twig', array('store' => $store, 'stores' => Store::getAll(), 'brands' => $store->getBrands(), 'all_brands' => Brand::getAll()));
+  });
 
   //DELETE all stores
 
