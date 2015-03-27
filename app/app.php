36dd2da7630169669e350f2523ed30509d2c45d5
Evan Butler <evanb2@mac.com>
2015-03-27 13:44:00 -0700

    write routes for READ stores and brands

--- app/app.php
+++ app/app.php
@@ -18,12 +18,26 @@
     return $app['twig']->render('index.twig');
   });
   //READ all stores
+  $app->get("/stores", function() use ($app) {
+    return $app['twig']->render('stores.twig', array('stores' => Store::getAll()));
+  });
 
   //READ all brands
+  $app->get("/brands", function() use ($app) {
+    return $app['twig']->render('brands.twig', array('brands' => Brand::getAll()));
+  });
 
   //READ singular store
+  $app->get("/stores/{id}", function($id) use ($app) {
+    $store = Store::find($id);
+    return $app['twig']->render('store.twig', array('store' => $store, 'brands' => $store->getBrands(), 'all_brands' => Brand::getAll()));
+  });
 
   //READ singular brand
+  $app->get("/brands/{id}", function($id) use ($app) {
+    $brand = Brand::find($id);
+    return $app['twig']->render('brand.twig', array('brand' => $brand, 'stores' => $brand->getStores(), 'all_stores' => Store::getAll()));
+  });
 
   //store EDIT form
 
